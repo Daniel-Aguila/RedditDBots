2b9bae0e0eba36844af948df2fe17102074697e3
Daniel Aguila <47072469+Daniel-Aguila@users.noreply.github.com>
2019-09-22 04:44:26 -0500

    Add files via upload

---
+++ Reddit/reddit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+import praw
+
+reddit = praw.Reddit('bot1')
+
+subreddit = reddit.subreddit("learnpython")
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
